344a6caa7b3773001ac4320881a8f111c18337ce
Hasnine670817 <hasninetorun@gmail.com>
2025-10-08 08:23:34 +0600

    problem-4 done

---
+++ problem-4.js
@@ -0,0 +1,17 @@
+/*
+Problem 4: Convert String to Uppercase
+
+Write a program to convert a string to uppercase manually (without using .toUpperCase() inside loop).
+Hint: Use .toUpperCase() once after reading the string.
+
+Sample Input: "hello"
+Sample Output: "HELLO"
+*/
+
+// Solution: 
+
+let word = "hello";
+
+let uppercaseText = word.toUpperCase();
+console.log(uppercaseText);
+// Output: HELLO
