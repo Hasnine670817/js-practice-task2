c6fcedf39520cc1d88606480267a903b9ea9e80f
Hasnine670817 <hasninetorun@gmail.com>
2025-10-08 08:45:41 +0600

    problem-7 done

---
+++ problem-7.js
@@ -0,0 +1,28 @@
+/*
+Problem 7: Print Each Character
+
+Write a program that prints each character of a string on a new line.
+Hint: Use a for loop to go through the string.
+
+Sample Input: "Code"
+Sample Output:
+C
+o
+d
+e
+*/
+
+// Solution: 
+
+const text = "Code";
+
+for (let i = 0; i < text.length; i++) {
+    console.log(text[i]);
+}
+
+/* Output: 
+C
+o
+d
+e
+*/
