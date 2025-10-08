452dedac7e577889d4d5ea756f8f2170568c4dfa
Hasnine670817 <hasninetorun@gmail.com>
2025-10-08 08:55:53 +0600

    problem-10 done

---
+++ problem-10.js
@@ -0,0 +1,30 @@
+/*
+Problem 10: Repeat Word Using Loop
+
+Write a program that prints a given word 5 times using a loop.
+Hint: Use for loop and console.log().
+
+Sample Input: "Hello"
+Sample Output:
+
+Hello
+Hello
+Hello
+Hello
+Hello
+
+*/
+
+let word = "Hello";
+
+for (let i = 0; i < 5; i++) {
+    console.log(word);
+}
+
+/* Output:
+Hello
+Hello
+Hello
+Hello
+Hello
+*/
